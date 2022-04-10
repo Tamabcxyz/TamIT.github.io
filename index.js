bd5ee6d17f97563bb73f8832f3fccdb84b3711b1
Trần Minh Tâm <46889933+Tamabcxyz@users.noreply.github.com>
2022-04-10 20:16:19 +0700

    release one
    troll ^^

---
+++ index.js
@@ -0,0 +1,12 @@
+var love = document.getElementById("love");
+var no_love =document.getElementById("no-love");
+
+love.addEventListener("click", myFunction);
+function loveFunction(){
+ alert('Tui biết mà, địa tui lâu lắm rồi mà =))')
+}
+
+no_love.addEventListener("mouseover",lovefunction);
+function noLovefunction(){
+   no_love.classList.toggle('move')
+}
